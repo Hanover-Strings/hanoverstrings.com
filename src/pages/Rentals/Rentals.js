a58d5bb9087fb05b460033e68eb3224e5f92a718
Christopher Goulet <fluxola500@gmail.com>
2018-10-10 21:17:35 -0400

    fixed copy and styling on lessons

--- src/pages/Rentals/Rentals.js
+++ src/pages/Rentals/Rentals.js
@@ -18,7 +18,7 @@ class Rentals extends React.Component {
             <h2>
               Make it easy
             </h2>
-            <h4>Whether you want to try a new and different instrument or just test your commitment to play without the burden of a purchase, come in to the shop  and rent one of our instruments. We also rent sound systems, lights and lasers for short or long term rentals from corporate events and weddings to wild parties. Rentals are easy, fun and affordable.  Come see us in person  and set up your rental. </h4>
+            <h4>Whether you want to try a new and different instrument or just test your commitment to play without the burden of a purchase, come in to the shop  and rent one of our instruments. We also rent sound systems, lights and lasers for short or long term rentals from corporate events and weddings to wild parties. Rentals are easy, fun and affordable.  Come see us at the shop or contact us via phone or e-mail to get a quote today! </h4>
           </Col>
           <Col xs='12' sm='12' md='12' lg='6'>
             {/* NEED BETTER IMAGE FOR CENTERING..... this one is too small and thus, looks off-center */}
