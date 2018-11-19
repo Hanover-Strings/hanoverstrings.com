57a3eedb8c2cc03e778dd9103e45cc63cef118cc
Christopher Goulet <fluxola500@gmail.com>
2018-11-19 11:05:03 -0500

    added hours to front page, rebuilt

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -36,6 +36,11 @@ class Home extends React.Component {
             <h4>In addition to our instrument retail, we also offer instrument repair and maintenance, live sound services, and audio system consultation and installation.</h4>
             <h4>If you are looking to pick our brains on a particular audio challenge or just want to strum one of our many guitars, please stop by for a visit while you're in town!</h4>
             <br />
+            <h3><strong>Hours:</strong></h3>
+            <h4><strong>Monday thru Friday | 9:30 - 5:30</strong></h4>
+            <h4><strong>Saturday | 10 - 3</strong></h4>
+            <h4><strong>CLOSED Sundays</strong></h4>
+            <br />
             <h4>Sign up for our mailing list to hear the latest!</h4>
             <MailchimpSubscribe className='fluid' url='https://hanoverstrings.us3.list-manage.com/subscribe/post?u=f74bbeb8cf613cbdece7061b1&amp;id=4e345d06b1'/>
             <a href='https://www.google.com/maps/search/?api=1&query=hanover+strings'><h1><i style={{color: 'green'}} class="fas fa-map-marked-alt"></i>   Link to map</h1></a>
