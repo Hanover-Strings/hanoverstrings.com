d17b0b02878b64d482ee63c84937b96dcb9c11e0
Davis McGraw <davismcgraw@dicks-MBP.localdomain>
2019-02-07 14:22:58 -0500

    updated policies on home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -48,13 +48,17 @@ class Home extends React.Component {
         </Row>
         <Row style={{textAlign: 'center', paddingTop: 24 }}>
           <Col xs='12' sm='12' md='12' lg={{size: 6, offset: 3}}>
-            <Button size='lg' onClick={this.toggle} style={{ marginBottom: '1rem' }}>Return Policy</Button>
+            <Button size='lg' onClick={this.toggle} style={{ marginBottom: '1rem' }}>Return and Trade-In Policies</Button>
             <Collapse isOpen={this.state.collapse}>
               <Card>
                 <CardBody>
                 <h4>If an item is returned in acceptable condition within 5 business days of purchase, we are willing to accept the return and process a refund reflecting the original payment method.</h4>
                 <h4>All returns at Hanover Strings, including those made within our stated 5-day return window, are accepted at our discretion. Hanover Strings reserves the right to remit payment in the form of store credit. Hanover Strings also reserves the right to collect a “restocking fee” for any returned item.</h4>
                 <h4>Special Order returns are accepted purely at our discretion and will be refunded with store credit only. Shipping fees associated with special orders will not be refunded under any circumstances.</h4>
+                <h4>*</h4>
+                <h4>On rare occasions and very much at our discretion, Hanover Strings will accept instrument trade-ins. We are not obligated to purchase any instrument considered for trade-in and will always advise our customers that a private sale via online marketplace or community board is their best option to sell their instruments.</h4>
+                <h4>If you have an instrument that you are interested in selling or trading, we strongly recommend that you call or e-mail us before bringing it in.</h4>
+                <h4>Hanover Strings does not offer consignment services to our customers.</h4>
                 </CardBody>
               </Card>
             </Collapse>
